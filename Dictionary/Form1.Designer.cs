f021c2d7dbfe0c3a9221afef9a9bd3bbf5ac0b67
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2021-10-07 20:14:39 +0100

    Add project files.

---
+++ Dictionary/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button5 = new System.Windows.Forms.Button();
+            this.buttonUnit5 = new System.Windows.Forms.Button();
+            this.buttonUnit4 = new System.Windows.Forms.Button();
+            this.buttonUnit3 = new System.Windows.Forms.Button();
+            this.buttonUnit2 = new System.Windows.Forms.Button();
+            this.buttonUnit1 = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanelSmall = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanelBig = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.HSplit;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.splitContainer1.Panel1.Controls.Add(this.button5);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonUnit5);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonUnit4);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonUnit3);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonUnit2);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonUnit1);
+            this.splitContainer1.Panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1143, 742);
+            this.splitContainer1.SplitterDistance = 64;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(1115, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(25, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "?";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // buttonUnit5
+            // 
+            this.buttonUnit5.Location = new System.Drawing.Point(480, 12);
+            this.buttonUnit5.Name = "buttonUnit5";
+            this.buttonUnit5.Size = new System.Drawing.Size(111, 40);
+            this.buttonUnit5.TabIndex = 4;
+            this.buttonUnit5.Text = "Unit 5";
+            this.buttonUnit5.UseVisualStyleBackColor = true;
+            this.buttonUnit5.Click += new System.EventHandler(this.buttonUnit5_Click);
+            // 
+            // buttonUnit4
+            // 
+            this.buttonUnit4.Location = new System.Drawing.Point(363, 12);
+            this.buttonUnit4.Name = "buttonUnit4";
+            this.buttonUnit4.Size = new System.Drawing.Size(111, 40);
+            this.buttonUnit4.TabIndex = 3;
+            this.buttonUnit4.Text = "Unit 4";
+            this.buttonUnit4.UseVisualStyleBackColor = true;
+            this.buttonUnit4.Click += new System.EventHandler(this.buttonUnit4_Click);
+            // 
+            // buttonUnit3
+            // 
+            this.buttonUnit3.Location = new System.Drawing.Point(246, 12);
+            this.buttonUnit3.Name = "buttonUnit3";
+            this.buttonUnit3.Size = new System.Drawing.Size(111, 40);
+            this.buttonUnit3.TabIndex = 2;
+            this.buttonUnit3.Text = "Unit 3";
+            this.buttonUnit3.UseVisualStyleBackColor = true;
+            this.buttonUnit3.Click += new System.EventHandler(this.buttonUnit3_Click);
+            // 
+            // buttonUnit2
+            // 
+            this.buttonUnit2.Location = new System.Drawing.Point(129, 12);
+            this.buttonUnit2.Name = "buttonUnit2";
+            this.buttonUnit2.Size = new System.Drawing.Size(111, 40);
+            this.buttonUnit2.TabIndex = 1;
+            this.buttonUnit2.Text = "Unit 2";
+            this.buttonUnit2.UseVisualStyleBackColor = true;
+            this.buttonUnit2.Click += new System.EventHandler(this.buttonUnit2_Click);
+            // 
+            // buttonUnit1
+            // 
+            this.buttonUnit1.Location = new System.Drawing.Point(12, 12);
+            this.buttonUnit1.Name = "buttonUnit1";
+            this.buttonUnit1.Size = new System.Drawing.Size(111, 40);
+            this.buttonUnit1.TabIndex = 0;
+            this.buttonUnit1.Text = "Unit 1";
+            this.buttonUnit1.UseVisualStyleBackColor = true;
+            this.buttonUnit1.Click += new System.EventHandler(this.buttonUnit1_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.splitContainer2.Panel1.Controls.Add(this.flowLayoutPanelSmall);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.BackColor = System.Drawing.Color.AliceBlue;
+            this.splitContainer2.Panel2.Controls.Add(this.flowLayoutPanelBig);
+            this.splitContainer2.Size = new System.Drawing.Size(1143, 674);
+            this.splitContainer2.SplitterDistance = 214;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // flowLayoutPanelSmall
+            // 
+            this.flowLayoutPanelSmall.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.flowLayoutPanelSmall.AutoScroll = true;
+            this.flowLayoutPanelSmall.Cursor = System.Windows.Forms.Cursors.Default;
+            this.flowLayoutPanelSmall.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanelSmall.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanelSmall.Name = "flowLayoutPanelSmall";
+            this.flowLayoutPanelSmall.Padding = new System.Windows.Forms.Padding(40, 15, 0, 0);
+            this.flowLayoutPanelSmall.Size = new System.Drawing.Size(214, 674);
+            this.flowLayoutPanelSmall.TabIndex = 0;
+            // 
+            // flowLayoutPanelBig
+            // 
+            this.flowLayoutPanelBig.Cursor = System.Windows.Forms.Cursors.Default;
+            this.flowLayoutPanelBig.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanelBig.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanelBig.Name = "flowLayoutPanelBig";
+            this.flowLayoutPanelBig.Size = new System.Drawing.Size(925, 674);
+            this.flowLayoutPanelBig.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 742);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Dictionary";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button buttonUnit5;
+        private System.Windows.Forms.Button buttonUnit4;
+        private System.Windows.Forms.Button buttonUnit3;
+        private System.Windows.Forms.Button buttonUnit2;
+        private System.Windows.Forms.Button buttonUnit1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelSmall;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelBig;
+    }
+}
+
---
+++ Dictionary/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ButtonDestroy()
+        {
+            flowLayoutPanelSmall.Controls.Clear();
+        }
+        private void ButtonMake(int n)
+        {
+            ButtonDestroy();
+            for (int i = 0; i < n; i++)
+            {
+                Button button = new Button();
+                button.Name = $"brutton{i}";
+                button.Text = $"Brutton_{i+1}";
+                button.Width = 116;
+                button.Height = 40;
+                flowLayoutPanelSmall.Controls.Add(button);
+            }
+        }
+
+        private void buttonUnit1_Click(object sender, EventArgs e)
+        {
+            ButtonMake(1);
+        }
+
+        private void buttonUnit2_Click(object sender, EventArgs e)
+        {
+            ButtonMake(2);
+        }
+
+        private void buttonUnit3_Click(object sender, EventArgs e)
+        {
+            ButtonMake(3);
+        }
+
+        private void buttonUnit4_Click(object sender, EventArgs e)
+        {
+            ButtonMake(4);
+        }
+
+        private void buttonUnit5_Click(object sender, EventArgs e)
+        {
+            ButtonMake(5);
+        }
+    }
+}
